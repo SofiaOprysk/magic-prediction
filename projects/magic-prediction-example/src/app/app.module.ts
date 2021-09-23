0f22d14b45483454bf95bea40b6b94df9c92467e
SofiaOprysk <1Sophiya1@gmail.com>
2021-09-23 14:03:31 +0300

    add lib from npm

--- projects/magic-prediction-example/src/app/app.module.ts
+++ projects/magic-prediction-example/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MagicPredictionsModule } from 'magic-predictions'
+import { MagicPredictionsModule } from '@oprysk.sofia/magic-predictions';
 
 import { AppComponent } from './app.component';
 
